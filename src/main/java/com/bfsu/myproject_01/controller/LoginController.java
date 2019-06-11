8cc7cfbecd20ec58b9d5dfee9bf2f321ea8083bc
GaoYang <2073646584@qq.com>
2019-06-11 17:11:15 +0800

    第二次 批量

---
+++ src/main/java/com/bfsu/myproject_01/controller/LoginController.java
@@ -0,0 +1,29 @@
+package com.bfsu.myproject_01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class LoginController {
+   /* @RequestMapping(value="/user/login",method = RequestMethod.POST)*/
+   @PostMapping("/user/login")
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password, Map<String,Object> map){
+
+        if (!StringUtils.isEmpty(password)&&password.equals("123456")){
+           //return "dashboard";
+            return  "redirect:/main.html";//加 /   表示绝对路径
+        }else{
+            map.put("msg","用户名或密码错误!");
+            return "login";
+        }
+
+    }
+
+
+}
--- src/main/java/com/bfsu/myproject_01/controller/Sucess.java
+++ src/main/java/com/bfsu/myproject_01/controller/Sucess.java
@@ -25,5 +25,10 @@ public class Sucess {
         map.put("users", Arrays.asList("zhangsan","lisi","wangwu","zhaoliu"));
         return "sucess";
     }
+    @RequestMapping({"login"})
+    public String index(){
 
+
+        return "login";
+    }
 }
